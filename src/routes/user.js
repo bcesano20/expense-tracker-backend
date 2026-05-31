ace9ed2459bb415c992b8b3a4f07e473ef2ebf5e
bcesano20 <54218077+bcesano20@users.noreply.github.com>
2026-05-31 11:18:04 +0200

    add new routes

---
+++ src/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Protected Routes
+router.get('/:id', authMiddleware, userController.getUserById);
+router.put('/:id', authMiddleware, userController.updateUser);
+router.delete('/:id', authMiddleware, userController.deleteUser);
+
+module.exports = router;
